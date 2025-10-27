2a7617a031ec97a06f10340b5ecbf84d30458a63
manologomez123 <145774876+manologomez123@users.noreply.github.com>
2025-10-27 13:39:28 +0100

    Cambio añadido por el profesor

--- holamanolo.java
+++ holamanolo.java
@@ -5,8 +5,10 @@ public class holamanolo {
 	public static void main(String[] args) {
 		
 System.out.println("hola soy Luis Sánchez");
-System.out.println("hola soy Julie Ayauca");		
+System.out.println("hola soy Julie Ayauca");	
+System.out.println("hola soy Manolo Gómez");
 	}
 
 }
+
 
